a639937bd07206bb37849020289af90dfbae5031
Min J <m04j00@gmail.com>
2022-06-18 18:20:38 +0900

    fix : index.html 연결 해제

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 const clients = [];
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    //res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
